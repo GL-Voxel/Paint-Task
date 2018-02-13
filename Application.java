33d2bb7853cb43b80bc7d57340e469505aa168f4
Sw0rdPenc1l <34251020+Sw0rdPenc1l@users.noreply.github.com>
2018-02-13 20:53:57 +0200

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Application extends JFrame{
+
+    Brush brush;
+
+    Application() {
+        super("ВЕЛОСИПЕД-ПЕИНТ");
+        brush = new Brush(Color.YELLOW);
+        setBounds(0, 0, 2000, 800);
+    }
+
+    void Run(){
+        addMouseListener(new Click());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        getContentPane().setBackground(Color.RED);
+        setVisible(true);
+    }
+
+    private class Click implements MouseListener {
+        public void mouseEntered(MouseEvent mouseEvent){
+
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent){
+            brush.Draw(mouseEvent.getX(), mouseEvent.getY()); //Рисуем точку
+        }
+
+        public void mousePressed(MouseEvent mouseEvent){
+
+        }
+
+        public void mouseReleased(MouseEvent mouseEvent){
+
+        }
+
+        public void mouseExited(MouseEvent click){
+
+        }
+    }
+
+    public static void main(String [] args){
+        Application paint = new Application();
+        paint.Run();
+    }
+}
